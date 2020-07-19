0053cee490f221a832e25012a1fa2748c745e765
jayp1 <jayp1@kpit.com>
2020-07-20 01:27:10 +0530

    Snake Pattern, Guide -pip,package and modules, iteratorsvslist, password generator, Assignment messsage generator

---
+++ List vs Iterator/list_vs_iterator.py
@@ -0,0 +1,9 @@
+sq_list = [x**2 for x in range(10)]  # this produces a list of squares
+
+sq_iterator = (x**2 for x in range(10))  # this produces an iterator of squares
+
+print(sq_list)
+print(sq_iterator)
+
+for items in sq_iterator:
+    print(items)
