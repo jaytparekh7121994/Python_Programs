fcb6302540218e91d70721eea366cbe9bf981def
jaytparekh7121994 <jaytparekh7121994@gmail.com>
2022-08-14 20:40:17 +0530

    Testcases for Stack functionality

--- Stack TDD/test_stack_tdd.py
+++ Stack TDD/test_stack_tdd.py
@@ -21,13 +21,19 @@ class StackTestCases(TestCase):
         self.stack.push(9)
         self.stack.pop()
         self.assertTrue(self.stack.isEmpty())
+        self.stack.push(3)
+        self.stack.push(9)
+        self.stack.pop()
+        self.assertEqual(self.stack.peek(), 3)
+        self.stack.pop()
+        self.assertTrue(self.stack.isEmpty())
 
     def test_peek(self):
         """ Test peek into the last item of stack """
         self.stack.push(8)
         self.stack.push(9)
         self.assertTrue(self.stack.peek(), 9)
-
+        
     def test_isEmpty(self):
         """ Test if the stack is empty """
         self.assertTrue(self.stack.isEmpty())
